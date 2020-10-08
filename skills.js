b3faa182d022fb7e8865229818c9cf8472fe1eaa
chaitali-09 <68439018+chaitali-09@users.noreply.github.com>
2020-10-08 09:08:48 +0530

    Add files via upload

---
+++ skills.js
@@ -0,0 +1,8 @@
+function select() {
+    
+        var select= document.getElementById("selectSkills").innerText;
+        document.getElementById("addSkills").innerText = select;
+
+        
+    
+}
